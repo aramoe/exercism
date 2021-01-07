e56bc453890a4345ce88dd5a08d64c3e33903d20
Moe Arashiba <moe.arashiba@gmail.com>
2021-01-07 21:41:04 +0900

    submit ruby/gigasecond
    https://exercism.io/my/solutions/32036ea9caa24bdeb38e2debcb2d7e5a

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,8 +1,4 @@
-=begin
-Write your code for the 'Gigasecond' exercise in this file. Make the tests in
-`gigasecond_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/gigasecond` directory.
-=end
-
+class Gigasecond
+  GIGA = 1e9
+  def self.from(time) = time + GIGA
+end
