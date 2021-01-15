6f775f01a8d501872cb329150dda55cbdc30ab08
Moe Arashiba <moe.arashiba@gmail.com>
2021-01-15 12:30:58 +0900

    submit ruby/resistor_color_duo #2
    https://exercism.io/my/solutions/e0e690bad9114830bbadde2772781d87

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,10 +1,9 @@
 class ResistorColorDuo
   COLORS = %w( black brown red orange yellow green blue violet grey white )
 
-  def self.value(names)
-    names[0, 2] # take duo
+  def self.value(names) =
+    names.take(2) # duo
       .map{|name| COLORS.index(name).to_s }
       .join('')
       .to_i
-  end
 end
