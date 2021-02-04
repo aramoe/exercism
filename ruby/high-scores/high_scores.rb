fc6b45f89d58cfaa44f27c243508b6478e513498
Moe Arashiba <moe.arashiba@gmail.com>
2021-02-05 02:04:54 +0900

    submit ruby/high-scores

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -1,8 +1,23 @@
-=begin
-Write your code for the 'High Scores' exercise in this file. Make the tests in
-`high_scores_test.rb` pass.
+class HighScores
+  attr_reader :scores
 
-To get started with TDD, see the `README.md` file in your
-`ruby/high-scores` directory.
-=end
+  def initialize(scores) =
+    @scores = scores
 
+  def latest =
+    scores.last
+
+  def personal_best =
+    personal_tops.first
+
+  def personal_top_three =
+    personal_tops.take(3)
+
+  def latest_is_personal_best? =
+    latest == personal_best
+
+  private
+
+  def personal_tops =
+    scores.sort.reverse
+end
