ba92a779ed92ef65bdb88856e58fec21c25badec
Moe Arashiba <moe.arashiba@gmail.com>
2021-02-05 14:27:48 +0900

    update

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,6 +1,5 @@
 class Pangram
   # https://exercism.io/tracks/ruby/exercises/pangram/solutions/55eff503e184408991d48f484a859a79
-  def self.pangram?(sentence)
+  def self.pangram?(sentence) =
     ([*'a'..'z'] - sentence.downcase.chars).empty?
-  end
 end
