d669ace8110ab785fb1a1ba6472360d7ee3cd387
Moe Arashiba <moe.arashiba@gmail.com>
2021-02-06 00:40:30 +0900

    submit ruby/space-age

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,8 +1,23 @@
-=begin
-Write your code for the 'Space Age' exercise in this file. Make the tests in
-`space_age_test.rb` pass.
+class SpaceAge
+  def initialize(seconds) =
+    @seconds = seconds
 
-To get started with TDD, see the `README.md` file in your
-`ruby/space-age` directory.
-=end
+  def on_earth =
+    @seconds / (365.25 * 86_400)
 
+  TABLE = {
+    Mercury: 0.2408467,
+    Venus: 0.61519726,
+    Mars: 1.8808158,
+    Jupiter: 11.862615,
+    Saturn: 29.447498,
+    Uranus: 84.016846,
+    Neptune: 164.79132
+  }
+
+  TABLE.each_pair do |pl, ey|
+    define_method("on_#{pl.downcase}") do
+      on_earth / ey
+    end
+  end
+end
