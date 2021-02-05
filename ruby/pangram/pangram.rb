48865771cf687313eb66f547f183b6f957ffc367
Moe Arashiba <moe.arashiba@gmail.com>
2021-02-05 14:10:54 +0900

    submit ruby/pangram

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,8 +1,10 @@
-=begin
-Write your code for the 'Pangram' exercise in this file. Make the tests in
-`pangram_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/pangram` directory.
-=end
-
+class Pangram
+  def self.pangram?(sentence)
+    sentence
+      .downcase
+      .chars
+      .grep(/[a-z]/)
+      .sort
+      .uniq == ('a'..'z').to_a
+  end
+end
